46772d3f80246d4fabd8c03270206b899a8dbed6
f h <falhamad2002@gmail.com>
2024-11-05 10:40:43 +0300

    Initial commit

---
+++ src/IPhoneSettings.java
@@ -0,0 +1,13 @@
+//Define the Singleton Class:
+public class IPhoneSettings {
+    
+    //Create a Static Instance:
+    private static IPhoneSettings instance;
+
+    //Make the constructor private
+    private IPhoneSettings() {
+
+    }
+
+
+}
