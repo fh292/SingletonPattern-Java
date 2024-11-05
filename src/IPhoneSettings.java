c01cbc09a5acf840e5312da690a355b576cb2694
f h <falhamad2002@gmail.com>
2024-11-05 13:17:50 +0300

    done

--- src/IPhoneSettings.java
+++ src/IPhoneSettings.java
@@ -1,13 +1,47 @@
 //Define the Singleton Class:
 public class IPhoneSettings {
-    
+
+    //Declare private variables
+    private int brightness;
+    private boolean wifi;
+
     //Create a Static Instance:
     private static IPhoneSettings instance;
 
     //Make the constructor private
     private IPhoneSettings() {
-
+        setBrightness(brightness);
     }
 
+    //Public Static Access Method:
+    public static IPhoneSettings getInstance() {
+        if (instance == null) {
+            instance = new IPhoneSettings();
+        }
+        return instance;
+    }
 
+    //Add Settings Methods:
+    public void setBrightness(int brightness) {
+        if (brightness>=0 && brightness<=100) {
+            this.brightness = brightness;
+        }else {
+            System.out.println("Invalid Brightness Range");
+        }
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public void enableWiFi(){
+        wifi = true;
+        System.out.println("WiFi Enabled");
+    }
+
+    public void disableWiFi(){
+        wifi = false;
+        System.out.println("WiFi Disabled");
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Test Your Singleton:
+        IPhoneSettings iphoneSettings = IPhoneSettings.getInstance();
+
+        iphoneSettings.setBrightness(100);
+        System.out.println("Brightness Level: "+ iphoneSettings.getBrightness());
+        iphoneSettings.disableWiFi();
+        iphoneSettings.enableWiFi();
     }
 }
